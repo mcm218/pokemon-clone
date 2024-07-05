2a8fb1415d5cf3b3926a8e134bb64016dc3b5569
Michael Muniz <mcmuniz218@gmail.com>
2024-07-05 07:29:50 -0400

    move data out of components folder

--- Assets/_Scripts/Pokemon/PokemonData.cs
+++ Assets/_Scripts/Pokemon/PokemonData.cs
@@ -30,4 +30,36 @@ namespace _Scripts.Pokemon
     }
 
 
+}using System;
+using System.Collections.Generic;
+using System.Runtime.Serialization;
+using Sirenix.OdinInspector;
+using UnityEngine;
+
+
+// MVC =
+// View -> GameObject
+// Controller -> MonoBehaviour -> PokemonController
+// Model -> ScriptableObject -> Pokemon
+
+namespace _Scripts.Pokemon
+{
+
+
+    [Serializable, CreateAssetMenu(fileName = "NewPokemonData", menuName = "Pokemon Data")]
+    public class PokemonData : SerializedScriptableObject
+    {
+        public     int         id       = 1;
+        public new string      name     = "Pokemon";
+        public     string      nickname = "Pokemon";
+        public     Stats       stats    = new Stats();
+        public     Sprite      sprite;
+        public     PokemonType type     = PokemonType.Normal;
+
+        public Stats EVsDrop = new Stats();
+
+        public List<BaseMove> learnableMoves = new List<BaseMove>();
+    }
+
+
 }
